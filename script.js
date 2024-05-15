5834430fbf2ba247e22b063f109db8268f5c68c9
Jonathan Copeland <jonathan.copeland@hotmail.co.uk>
2024-05-15 09:24:30 +0100

    Added js

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
+let buttonContainer = document.querySelector("#number-buttons");
 
+createNumberButtons();
+
+function createNumberButtons(){
+
+    for(let i = 0; i < 3; i++){
+
+        let buttonRow = document.createElement("div");
+
+        buttonRow.classList.add("row");
+
+        buttonContainer.appendChild(buttonRow);
+
+        for(let j = 0; j < gridSizeValue; j++){
+            let gridCell = document.createElement("div");
+
+            gridCell.classList.add("cell");
+
+            gridRow.appendChild(gridCell);
+
+            gridCell.addEventListener("mouseover", function(e){
+                if(e.buttons == 1 || e.buttons == 3){
+                    this.classList.add("shade");
+                }
+            });
+        }
+
+    }
+};
